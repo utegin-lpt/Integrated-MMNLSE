3414d4197c845cf9368a9c9d2f8d0e8d4556348d
Azka Maula Iskandar Muda <49232129+amiskandarmuda@users.noreply.github.com>
2025-11-26 10:08:19 +0300

    Delete src/gmmnlse directory

--- src/gmmnlse/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Utility modules for GMMNLSE solver."""
-
-# Note: Plotter is not imported here to avoid circular imports.
-# Import directly from utils.plotter if needed: from gmmnlse.utils.plotter import Plotter
-
-__all__ = []
-
-
